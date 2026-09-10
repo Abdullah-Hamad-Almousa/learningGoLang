85f58044c51e4d4501963d44c3d8f5bf9fe8300d
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-10 11:21:54 +0300

    Arrays and Slices

--- main.go
+++ main.go
@@ -4,22 +4,33 @@ import "fmt"
 
 func main() {
 
-	var age int8 = 27
-	fName := "Abdullah"
-	lName := "Almousa"
-	var experince byte = 3
+	//Arrays and Slices
 
-	fmt.Println("Hello", fName, lName, "with experince of:", experince, "years and age:", age)
-	fmt.Printf("Hello %v %v with experince of: %v years and age: %v \n", fName, lName, experince, age)
-	fmt.Printf("Hello %q %q with experince of: %q years and age: %q \n", fName, lName, experince, age)
-	fmt.Printf("Age is of type %T\n", age)
-	fmt.Printf("you scored %f points! \n", 99.4)
-	fmt.Printf("you scored %.2f points! \n", 99.4)
+	//Arrays
+	var ages [3]int = [3]int{25, 27, 29}
 
-	// Sprintf
+	names := [4]string{"Ali,", "Hamad", "Fahad", "Mohammad"}
+	names[0] = "Ahmed"
 
-	var strOne = fmt.Sprint("Hello ", fName, " ", lName, " with experince of: ", experince, " years and age: ", age)
-	var strTwo = fmt.Sprintf("Hello", fName, lName, "with experince of:", experince, "years and age:", age)
-	fmt.Println("String have been saved:", strOne, "\n", strTwo)
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
 
+	//Slices
+	var scores = []int{93, 95, 99}
+	scores[2] = 97
+	scores = append(scores, 99) //append is overwrite on the Slices and adding a new value with it
+
+	fmt.Println(scores, len(scores))
+
+	//Slices ranges
+
+	rangeOne := names[1:3]  //Print between index 1 and 3. 1 is gonna print but it not gonna print 3
+	rangeTwo := names[2:]   //Print everything on index 1 to the last
+	rangeThree := names[:2] //Print everything from the start until index 2
+	fmt.Println(rangeOne)
+	fmt.Println(rangeTwo)
+	fmt.Println(rangeThree)
+
+	rangeOne = append(rangeOne, "Abdullah") //Is not gonna add it to array names it only gonna add it to rangeOne
+	fmt.Println(rangeOne)
 }
