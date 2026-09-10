48001116b89ebc92e643fcc0644da61d088cf107
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-10 16:50:50 +0300

    functions

--- main.go
+++ main.go
@@ -1,37 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(n string) {
+
+	fmt.Printf("Good morning %v \n", n)
+
+}
+
+func sayBye(n string) {
+
+	fmt.Printf("Goodbye %v \n", n)
+
+}
+
+func cycleNames(n []string, f func(string)) {
+
+	for _, v := range n {
+		f(v)
+	}
+
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
 
 func main() {
 
-	//Booleans and conditionals
+	//Functions
 
-	age := 45
+	sayGreeting("Abdullah")
+	sayBye("Abdullah")
 
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 45)
-	fmt.Println(age != 50)
+	cycleNames([]string{"Ahmed", "Ali", "Abdullah"}, sayGreeting)
+	cycleNames([]string{"Ahmed", "Ali", "Abdullah"}, sayBye)
 
-	if age < 30 {
-		fmt.Println("Age is less than 30")
-	} else if age < 40 {
-		fmt.Println("Age is less than 40")
-	} else {
-		fmt.Println("Age is not less than 45")
-	}
-
-	names := []string{"John", "Paul", "George", "Ringo", "Ali"}
-
-	for index, value := range names { //This is use for blacklist people, keys etc...
-		if index == 1 {
-			fmt.Println("Continuing at pos", index)
-			continue
-		} else if index == 2 {
-			fmt.Println("Breaking at pos", index)
-			break
-		}
-		fmt.Printf("The value at pos %v is %v \n", index, value)
-	}
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+	fmt.Printf("Circle 1 is %.3f, and the circle 2 is %.3f", a1, a2)
+	fmt.Printf("Circle 1 is %.3v, and the circle 2 is %.3v", a1, a2)
 
 }
