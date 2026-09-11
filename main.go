f6934b326f1defa823c04eefecfffcf2f076ec5a
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-11 18:51:22 +0300

    Structs and custom / interface

--- main.go
+++ main.go
@@ -2,21 +2,10 @@ package main
 
 import "fmt"
 
-func updateNames(x *string) {
-	*x = "Ali"
-}
-
 func main() {
-	//only about Pointers
+	//Structs and custom
 
-	name := "Ahemd"
+	myBill := newBill("Abdullah's bill")
 
-	m := &name
-	//fmt.Println("Memeory address:", m)
-	//fmt.Println("The value of the memory address is:", *m)
-	//What we just do is make m as a pointer to use it with name to change it from type A to be a type B, to be a pointer
-	fmt.Println(name)
-	updateNames(m)
-	fmt.Println(name)
-
+	fmt.Println(myBill)
 }
