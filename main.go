4a00fd8258808257d0de5ad45e43757cdacdf3ec
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-11 13:36:15 +0300

    Maps

--- main.go
+++ main.go
@@ -2,18 +2,41 @@ package main
 
 import "fmt"
 
-var scores = 99.94
-
 func main() {
+	//Maps
 
-	//Package Scope
-
-	sayHello("Abdullah-Almousa")
-
-	for _, v := range points {
-		fmt.Printf("%v \t", v)
+	menu := map[string]float32{
+		"pie":   24.3,
+		"water": 2.1,
+		"soup":  15.2,
+		"salad": 18.2,
+		"cream": 9.1,
 	}
 
-	showScores()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	for k, v := range menu { // k will hold the string as index and the v will hold the price of the string
+		fmt.Println(k, ":", v)
+	}
+
+	//ints as key
+
+	phonebook := map[int]string{
+		911: "Emergency",
+		999: "Police",
+		998: "Civil Defense",
+		997: "Medical services",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[911])
+
+	phonebook[911] = "Superman"
+	fmt.Println(phonebook[911])
+
+	for k, v := range phonebook {
+		fmt.Println(k, ":", v)
+	}
 
 }
