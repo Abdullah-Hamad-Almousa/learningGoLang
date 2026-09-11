2e1f8b3ad3c7673f155714bf84142958654a222a
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-11 14:43:27 +0300

    only about pointers how to make a type A to change values using function without return

--- main.go
+++ main.go
@@ -2,42 +2,21 @@ package main
 
 import "fmt"
 
-func updateName(x string) string { //It takes a copy of original variable that why it gonna print a Ahmed
-	x = "Fahad"
-
-	return x
-}
-
-func updateMenu(y map[string]int8) { //It gonna pass the original variable that why it gonna print the new value
-	y["Coffee"] = 10
+func updateNames(x *string) {
+	*x = "Ali"
 }
 
 func main() {
-	//Passing
-	//Group type A -> strings, int, float, bools, arrays and structs
+	//only about Pointers
 
-	//name := "Ahemd"
-	//
-	//name = updateName(name)
-	//
-	//fmt.Println(name)
+	name := "Ahemd"
 
-	//Group type B -> slice, maps and functions
-
-	menu := map[string]int8{
-		"Pie":       24,
-		"Salad":     18,
-		"Ice cream": 2,
-	}
-
-	updateMenu(menu)
-
-	for k, v := range menu {
-		fmt.Println(k, v)
-	}
-
-	//Type A can't be changed from outside it area unless used return
-	//Type B can be changed from outside it area
-	//Type A can be called non-pointer values and B can be called pointer values
+	m := &name
+	//fmt.Println("Memeory address:", m)
+	//fmt.Println("The value of the memory address is:", *m)
+	//What we just do is make m as a pointer to use it with name to change it from type A to be a type B, to be a pointer
+	fmt.Println(name)
+	updateNames(m)
+	fmt.Println(name)
 
 }
